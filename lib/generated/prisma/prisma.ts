0a8ada4cc0e0ba19ced8ae87ef2dedeada982aeb
niecki tehrany azad <niecki.tehrany.bildung@gmal.com>
2026-01-10 22:53:46 +0100

    generate prisma-link prisma to client and config prisma output for this address

--- lib/generated/prisma/prisma.ts
+++ lib/generated/prisma/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client.ts";
+import { PrismaClient } from "./client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
